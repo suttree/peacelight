c4a1d4358f3671944dd1d6e58f2e630235bfd55a
Duncan Gough <duncan@suttree.com>
2021-10-14 10:46:08 +0100

    Adjust brightness with time of day

--- peacelight.py
+++ peacelight.py
@@ -12,20 +12,20 @@ height: Height of the area (default is 7)
 from noise import pnoise1
 from numpy import interp
 import scrollphathd
+import datetime
 import time
 import math
 
 def peace():
 	i = 1
 	step = 1
-	brightness = 0
+	brightness = 0.0
 
 	while True:
 		scrollphathd.clear()
 
-		brightness = interp(i, [-1, 1], [0,1]) # map the brightness between 0 and 1
+		brightness = interp(i, [-1.0, 1.0], [0.0, 1.0]) # map the brightness between 0 and 1
 		scrollphathd.fill(brightness, 0, 0, i, scrollphathd.height)
-
 		scrollphathd.show()
 
 		# SLEEP
@@ -38,9 +38,16 @@ def peace():
 				step = -1
 			else:
 				step = 1
-
 		i = i + step
 		
+		# Map brightness to hour of day
+		hour = datetime.now().hour
+		if (hour >= 11 and hour <= 13):
+			brightness = 1.0
+		elif (hour < 11 and hour > 0):
+			brigthness = brightness + 0.09
+		elsif (hour > 13 and hour < 24):
+			brightness = brightness - 0.9
 
 def mapRange(value, inMin, inMax, outMin, outMax):
 	return outMin + (((value - inMin) / (inMax - inMin)) * (outMax - outMin))
