93cb27d48ddddbe800147b924d914f47cbc4b811
Duncan <duncan@suttree.com>
2021-10-14 11:16:58 +0100

    perlin light changes

--- peacelight.py
+++ peacelight.py
@@ -9,45 +9,51 @@ width: Width of the area (default is 17)
 height: Height of the area (default is 7)
 '''
 
+from datetime import datetime
 from noise import pnoise1
 from numpy import interp
 import scrollphathd
-import datetime
+import random
 import time
 import math
 
 def peace():
 	i = 1
 	step = 1
+	curr_hour = 0
 	brightness = 0.0
+	seed = random.uniform(0.1, 1.0)
 
 	while True:
 		scrollphathd.clear()
 
-		brightness = interp(i, [-1.0, 1.0], [0.0, 1.0]) # map the brightness between 0 and 1
-		scrollphathd.fill(brightness, 0, 0, i, scrollphathd.height)
+		#brightness = interp(i, [-1.0, 1.0], [0.0, 1.0]) # map the brightness between 0 and 1
+		b = interp(brightness, [-1.0, 1.0], [0.0, 1.0]) # map the brightness between 0 and 1
+		scrollphathd.fill(b, 0, 0, i, scrollphathd.height)
 		scrollphathd.show()
 
-		# SLEEP
-		time.sleep( 0.75 + float(pnoise1(i)) * 5.0 )
+		# Sleep
+		time.sleep( abs(0.45 + float( pnoise1( i * seed ) ) * 4.0) )
 
-		# CONTROL FLOW
+		# Control flow
 		if (i >= scrollphathd.width or i <= 0):
-			time.sleep(2.25 + float(pnoise1(i)) * 10.0 )
+			time.sleep( abs(2.25 + float(pnoise1( i * seed ) ) * 9.0 ) )
 			if step == 1:
 				step = -1
 			else:
 				step = 1
 		i = i + step
 		
-		# Map brightness to hour of day
+		# Map brightness to hour of day, each hour
 		hour = datetime.now().hour
-		if (hour >= 11 and hour <= 13):
-			brightness = 1.0
-		elif (hour < 11 and hour > 0):
-			brigthness = brightness + 0.09
-		elsif (hour > 13 and hour < 24):
-			brightness = brightness - 0.9
+		if (curr_hour <> hour):
+			curr_hour = hour
+			if (hour >= 11 and hour <= 13):
+				brightness = 1.0
+			elif (hour < 11 and hour > 0):
+				brightness = brightness + 0.09
+			elif (hour > 13 and hour < 24):
+				brightness = brightness - 0.9
 
 def mapRange(value, inMin, inMax, outMin, outMax):
 	return outMin + (((value - inMin) / (inMax - inMin)) * (outMax - outMin))
