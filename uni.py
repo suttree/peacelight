c1246fcd8ad9074dabf9e90c57ef04d19bf00f6c
Duncan Gough <duncan@suttree.com>
2022-02-17 17:52:55 +0000

    remove old code

--- uni.py
+++ uni.py
@@ -8,14 +8,12 @@ uh = UnicornHATMini()
 uh.set_brightness(0.5)
 
 while True:
-	for x in range(17):
-		for y in range(7):
-			uh.set_pixel(x, y, 0, 255, 255)
+    #for x in range(17):
+    #    for y in range(7):
+    #        uh.set_pixel(x, y, 0, 255, 255)
 
-	uh.set_pixel(0, 0, 255, 0, 0)
-	uh.show()
-
-
+    #uh.set_pixel(0, 0, 255, 0, 0)
+    #uh.show()
 
     for x in range(7):
         # morning: 0-5
