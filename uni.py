dc7df6ed0dd60bbafd025563c4806fa7b937525c
duncan <duncan@suttree.com>
2022-02-17 19:20:38 +0000

    time of day, random hue

--- uni.py
+++ uni.py
@@ -1,32 +1,42 @@
 #!/usr/bin/env python3
-import time
+import time, datetime
 from colorsys import hsv_to_rgb
 
 from unicornhatmini import UnicornHATMini
 uh = UnicornHATMini()
 
-uh.set_brightness(0.5)
+uh.set_brightness(0.1)
 
 while True:
-    #for x in range(17):
-    #    for y in range(7):
-    #        uh.set_pixel(x, y, 0, 255, 255)
+    # morning: 0-5
+    # afternoon: 6-12
+    # evening: 13-17
+    hour = datetime.datetime.now().hour
+    #print(hour)
+    #print('---')
 
-    #uh.set_pixel(0, 0, 255, 0, 0)
-    #uh.show()
+    if hour in range(6, 12):
+        start, end = 0, 5
+        uh.set_brightness(0.1)
+    elif hour in range(12, 18):
+        start, end = 6, 12
+        uh.set_brightness(0.5)
+    elif hour in range(18, 23):
+        start, end = 13, 16
+        uh.set_brightness(0.1)
+    else:
+        start, end = 0, 0
+        uh.set_brightness(0.0)
+    #print(start, end)
 
-    for x in range(7):
-        # morning: 0-5
-        # afternoon: 6-12
-        # evening: 13-17
-        start = 0
-        end = 5
-        
-        #hue = (time.time() / 10.0)
-        #r, g, b = [int(c * 255) for c in hsv_to_rgb(hue, 1.0, 1.0)]
-        
-        r, g, b, = 0, 255, 255
-        for y in range(start..end):
+    for x in range(start, end):
+        hue = (time.time() / 1000.0)
+        r, g, b = [int(c * 255) for c in hsv_to_rgb(hue, 1.0, 1.0)]
+
+        for y in range(7):
+            print(x, y)
             uh.set_pixel(x, y, r, g, b)
 
-    time.sleep(1.0 / 60)
+    uh.show()
+
+    time.sleep(30)
