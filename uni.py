4125af43c71b02b6e7d16c327d430277ff985c74
Duncan Gough <duncan@suttree.com>
2022-02-17 09:49:21 +0000

    psuedo code for dynamic 24hr lighting

--- uni.py
+++ uni.py
@@ -14,3 +14,21 @@ while True:
 
 	uh.set_pixel(0, 0, 255, 0, 0)
 	uh.show()
+
+
+
+    for x in range(7):
+        # morning: 0-5
+        # afternoon: 6-12
+        # evening: 13-17
+        start = 0
+        end = 5
+        
+        #hue = (time.time() / 10.0)
+        #r, g, b = [int(c * 255) for c in hsv_to_rgb(hue, 1.0, 1.0)]
+        
+        r, g, b, = 0, 255, 255
+        for y in range(start..end):
+            uh.set_pixel(x, y, r, g, b)
+
+    time.sleep(1.0 / 60)
