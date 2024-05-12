8bdab8d6a0ed71f3fe39846aff096a0b251ebca3
Duncan Gough <duncan@suttree.com>
2024-05-12 12:10:23 +0100

    added time-related guard clause

--- peacelight.py
+++ peacelight.py
@@ -25,43 +25,49 @@ def peace():
 	seed = random.uniform(0.1, 1.0)
 
 	while True:
-		scrollphathd.clear()
+        now = datetime.datetime.now()
+        current_time = now.time()
+        start_time = datetime.time(18, 0)  # 6:00 PM
+        end_time = datetime.time(23, 59, 59)  # Midnight
 
-		#brightness = interp(i, [-1.0, 1.0], [0.0, 1.0]) # map the brightness between 0 and 1
-		b = interp(brightness, [-1.0, 1.0], [0.0, 1.0]) # map the brightness between 0 and 1
-		scrollphathd.fill(b, 0, 0, i, scrollphathd.height)
-		scrollphathd.show()
+        if start_time <= current_time <= end_time:
+            scrollphathd.clear()
 
-		# Sleep
-		time.sleep( abs(0.45 + float( pnoise1( i * seed ) ) * 4.0) )
+            #brightness = interp(i, [-1.0, 1.0], [0.0, 1.0]) # map the brightness between 0 and 1
+            b = interp(brightness, [-1.0, 1.0], [0.0, 1.0]) # map the brightness between 0 and 1
+            scrollphathd.fill(b, 0, 0, i, scrollphathd.height)
+            scrollphathd.show()
 
-		# Control flow
-		if (i >= scrollphathd.width or i <= 0):
-			time.sleep( abs(5.25 + float(pnoise1( i * seed ) ) * 50.0 ) )
-			if step == 1:
-				step = -1
-			else:
-				step = 1
-		i = i + step
-		
-		# Map brightness to hour of day, each hour
-		hour = datetime.now().hour
-		if (curr_hour <> hour):
-			curr_hour = hour
-			if (hour >= 11 and hour <= 14):
-				brightness = 1.0 # brightest around noon
-			elif (hour < 11 and hour > 0):
-				brightness = brightness + 0.09 # taper up towards noon
-			elif (hour > 14 and hour <= 23):
-				brightness = brightness - 0.11 # taper down towards midnight
-			else:
-				brightness = -1.0 # erk, off
+            # Sleep
+            time.sleep( abs(0.45 + float( pnoise1( i * seed ) ) * 4.0) )
 
-			# Ensure we max out at +=1.0, for interpolation later on
-			if brightness > 1.0:
-				brightness = 1.0
-			elif brightness < -1.0:
-				brightness = -1.0
+            # Control flow
+            if (i >= scrollphathd.width or i <= 0):
+                time.sleep( abs(5.25 + float(pnoise1( i * seed ) ) * 50.0 ) )
+                if step == 1:
+                    step = -1
+                else:
+                    step = 1
+            i = i + step
+            
+            # Map brightness to hour of day, each hour
+            hour = datetime.now().hour
+            if (curr_hour <> hour):
+                curr_hour = hour
+                if (hour >= 11 and hour <= 14):
+                    brightness = 1.0 # brightest around noon
+                elif (hour < 11 and hour > 0):
+                    brightness = brightness + 0.09 # taper up towards noon
+                elif (hour > 14 and hour <= 23):
+                    brightness = brightness - 0.11 # taper down towards midnight
+                else:
+                    brightness = -1.0 # erk, off
+
+                # Ensure we max out at +=1.0, for interpolation later on
+                if brightness > 1.0:
+                    brightness = 1.0
+                elif brightness < -1.0:
+                    brightness = -1.0
 
 def mapRange(value, inMin, inMax, outMin, outMax):
 	return outMin + (((value - inMin) / (inMax - inMin)) * (outMax - outMin))
